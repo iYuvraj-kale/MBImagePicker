625981d7d5c0957326a6b5564cbe8af9ad7007aa
Yuvraj Bharat Kale <yuvraj.kale@mindbowser.com>
2020-12-12 16:30:41 +0530

    first commit

---
+++ MBImagePickerTests/MBImagePickerTests.swift
@@ -0,0 +1,33 @@
+//
+//  MBImagePickerTests.swift
+//  MBImagePickerTests
+//
+//  Created by Yuvraj Bharat Kale on 12/12/20.
+//
+
+import XCTest
+@testable import MBImagePicker
+
+class MBImagePickerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
